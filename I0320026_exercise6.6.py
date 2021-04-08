fa07903518d058a12985751c435ff7fadbf23b50
Dhea Naomi <dheanaomi21@gmail.com>
2021-04-08 13:42:31 +0700

    pengumpulan tugas

---
+++ I0320026_exercise6.6.py
@@ -0,0 +1,6 @@
+#menggunakan for pada tipe list
+for matkul in ["Kalkulus", "Fisika", "Kimia"]:
+    print(matkul)
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dhea-Naomi_I0320026_Aditya_Tugas6
+Repository ini berisi pengumpulan Tugas 6 Programa Komputer
